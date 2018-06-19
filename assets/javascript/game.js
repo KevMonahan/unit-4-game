8654fc5e0aae669ddba250711d98f453875096da
KMonahan <kevin.monahan@hotmail.com>
2018-06-18 23:48:15 -0500

    incredibly irritated i cannot figure out what is causing my defenderDiv to populate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,131 @@
-$(document).ready(function(){
-    var character1 = {
+$(document).ready(function () {
+
+
+    var Lurker = {
+        type: 'Lurker',
         'Health Points': 200,
         'Attack Power': 5,
         'Computer Attack Power': 20,
-        'playerPickedUnit': false
+        'playerPickedUnit': false,
+        'enemyPickedUnit': false,
+        'stillAlive': true,
+        "imgurl": `src="assets/Images/Lurker_SCR_HeadAnim.gif"`
     }
-    var character2 = {
+    var Hydralisk = {
+        type: 'Hydralisk',
         'Health Points': 175,
         'Attack Power': 6,
         'Computer Attack Power': 15,
-        'playerPickedUnit': false
+        'playerPickedUnit': false,
+        'enemyPickedUnit': false,
+        'stillAlive': true,
+        "imgurl": `src="assets/Images/Hydralisk.gif"`
     }
-    var character3 = {
+    var Zergling = {
+        type: 'Zergling',
         'Health Points': 150,
         'Attack Power': 7,
         'Computer Attack Power': 10,
-        'playerPickedUnit': false
+        'playerPickedUnit': false,
+        'enemyPickedUnit': false,
+        'stillAlive': true,
+        "imgurl": `src="assets/Images/Zergling_SCR_HeadAnim.gif"`
     }
-    var character4 = {
+    var Rhynadon = {
+        type: 'Rhynadon',
         'Health Points': 120,
         'Attack Power': 8,
         'Computer Attack Power': 5,
-        'playerPickedUnit': false
+        'playerPickedUnit': false,
+        'enemyPickedUnit': false,
+        'stillAlive': true,
+        "imgurl": `src="assets/Images/Rhynadon_SCR_HeadAnim.gif"`
     }
 
-    var allCharacters = [character1, character2, character3, character4];
+    var allCharacters = [Rhynadon, Zergling, Hydralisk, Lurker];
     var playerCharacter = "";
-    var unpickedCharacters = [];
+    var enemyCharacters = [];
+    var defender = "";
     var playerAttackPower = 0;
     var playerHealthPoints = 0;
     var enemyHealthPoints = 0;
+    var defenderDamage = 0;
+    var defenderHealth = 0;
 
-    function pickCharacter() {
+    for (var character in allCharacters) {
+        var characterCard = '<div class="col-md-3"><div class="card startingZerg" style="width: 18rem;" value="' + allCharacters[character].type + '">'
+        characterCard += `<img class="card-img-top" ` + allCharacters[character].imgurl + ` alt="Character Image">`
+        characterCard += `<div class="card-img-overlay">`
+        characterCard += `<h5 class="card-title">` + allCharacters[character].type + `</h5>`
+        characterCard += `<p class="card-text"> Health Points = ` + allCharacters[character]["Health Points"] + `</p>`
+        characterCard += `<p class="card-text"> Attack Power = ` + allCharacters[character]["Attack Power"] + `</p>`
+
+
+        $("#allCharacters").append(characterCard);
+    }
+    $("#enemyDiv").on("click", ".enemies", function () {
+
+
+        var getDefender = $(this).attr("value");
+
+
+        defender = getDefenderByType(getDefender);
+        console.log(defender);
+        populateDefender(defender);
+
+
+    });
+
+    $(".startingZerg").on("click", function () {
+
+        var getPlayer = $(this).attr("value");
+
+        playerCharacter = getCharacterByType(getPlayer);
+
+
+
+        var playerCard = '<div class="col-md-3"><div class="card" style="width: 18rem;" value="' + playerCharacter.type + '">'
+        playerCard += '<img class="card-img-top" ' + playerCharacter.imgurl + 'alt="Character Image">'
+        playerCard += '<div class="card-img-overlay">'
+        playerCard += '<h5 class="card-title">' + playerCharacter.type + '</h5>'
+        playerCard += '<p class="card-text"> Health Points = ' + playerCharacter["Health Points"] + '</p>'
+        playerCard += '<p class="card-text"> Attack Power = ' + playerCharacter["Attack Power"] + '</p>'
+
+        populateEnemyCharacters(allCharacters);
+        $("#playerDiv").append(playerCard);
+        $("#allCharacters").hide();
+        $("#intro").hide();
+    });
+
+
+
+    function getCharacterByType(type) {
+        for (characters in allCharacters) {
+            if (allCharacters[characters].type === type) {
+                playerCharacter = allCharacters[characters];
+                playerCharacter.playerPickedUnit = true;
+            }
+        }
+        return playerCharacter;
+    }
+
+
+    function getDefenderByType(type) {
+        for (enemies in enemyCharacters) {
+            if (enemyCharacters[enemies].type === type) {
+                defender = enemyCharacters[enemies];
+                defender.enemyPickedUnit = true;
+            }
+        }
+        return defender;
 
     }
 
-    function pickEnemy() {
-
-    }
-
-    function attackEnemy() {
-
+    function attackEnemy(playerCharacter, defender) {
+        playerAttackPower = playerCharacter["Attack Power"];
+        playerHealthPoints = playerCharacter["Health Points"];
+        defenderDamage = defender["Computer Attack Power"];
+        defenderHealth = defender["Health Points"]
     }
 
     function resetGame() {
@@ -48,9 +133,77 @@ $(document).ready(function(){
         $("#enemyDiv").empty();
     }
 
+    function populateEnemyCharacters(allCharacters) {
+        $("#enemyDiv").empty();
+        for (var characters in allCharacters) {
+            if (allCharacters[characters].playerPickedUnit === false && allCharacters[characters].stillAlive === true) {
+                enemyCharacters.push(allCharacters[characters]);
+            }
+        }
+        for (var unpicked in enemyCharacters) {
 
-    
+            var unpickedCards = '<div class="col-md-3"><div class="card enemies" style="width: 18rem;" value="' + enemyCharacters[unpicked].type + '">'
+            unpickedCards += '<img class="card-img-top" ' + enemyCharacters[unpicked].imgurl + ' alt="Character Image">'
+            unpickedCards += '<div class="card-img-overlay">'
+            unpickedCards += '<h5 class="card-title">' + enemyCharacters[unpicked].type + '</h5>'
+            unpickedCards += '<p class="card-text"> Health Points = ' + enemyCharacters[unpicked]["Health Points"] + '</p>'
+            unpickedCards += '<p class="card-text"> Attack Power = ' + enemyCharacters[unpicked]["Attack Power"] + '</p>'
 
+            $("#enemyDiv").append(unpickedCards);
+        }
+    }
 
+    function populateDefender(defender) {
+        var defenderCard = '<div class="col-md-3"><div class="card" style="width: 18rem;" value="' + defender.type + '">'
+        defenderCard += '<img class="card-img-top" ' + defender.imgurl + ' alt="Character Image">'
+        defenderCard += '<div class="card-img-overlay">'
+        defenderCard += '<h5 class="card-title">' + defender.type + '</h5>'
+        defenderCard += '<p class="card-text"> Health Points = ' + defender["Health Points"] + '</p>'
+        defenderCard += '<p class="card-text"> Attack Power = ' + defender["Attack Power"] + '</p>'
+
+        $("#defenderDiv").append(defenderCard);
+    }
 
 })
+    // for (var character in allCharacters) {
+
+    //     if (allCharacters[character].playerPickedUnit === false) {
+
+    //         var playerCard = `<div class="col-md-3"><div class="card" style="width: 18rem;">`
+    //         playerCard += `<img class="card-img-top" ` + playerCharacter.imgurl + 'alt="Character Image">'
+    //         playerCard += '<div class="card-body>'
+    //         playerCard += `<h5 class="card-title">` + playerCharacter.type + `</h5>`
+    //         playerCard += `<p class="card-text"> Health Points = ` + allCharacters[character]["Health Points"] + `</p>`
+    //         playerCard += `<p class="card-text"> Attack Power = ` + allCharacters[character]["Attack Power"] + `</p>`
+
+    //         $("#playerDiv").append(playerCard);
+    //     }
+    // if (allCharacters[character].playerPickedUnit === false) {
+    //     enemyCharacters.push(allCharacters[character]);
+
+    //     for (var unpicked in enemyCharacters) {
+
+    //         var unpickedCards = `<div class="col-md-3"><div class="card" style="width: 18rem;">`
+    //         unpickedCards += `<img class="card-img-top" ` + enemyCharacters[unpicked].imgurl + 'alt="Character Image">'
+    //         unpickedCards += '<div class="card-body>'
+    //         unpickedCards += `<h5 class="card-title">` + enemyCharacters[unpicked].type + `</h5>`
+    //         unpickedCards += `<p class="card-text"> Health Points = ` + enemyCharacters[unpicked]["Health Points"] + `</p>`
+    //         unpickedCards += `<p class="card-text"> Attack Power = ` + enemyCharacters[unpicked]["Attack Power"] + `</p>`
+
+    //         $("#enemyDiv").append(unpickedCards);
+
+    // Rhynadon["playerPickedUnit"] = true;
+
+    // console.log(check);
+    // var playerCard = `<div class="col-md-3"><div class="card" style="width: 18rem;">`
+    // playerCard += `<img class="card-img-top" ` + playerCharacter.imgurl + 'alt="Character Image">'
+    // playerCard += '<div class="card-body>'
+    // playerCard += `<h5 class="card-title">` + playerCharacter.type + `</h5>`
+    // playerCard += `<p class="card-text"> Health Points = ` + allCharacters[character]["Health Points"] + `</p>`
+    // playerCard += `<p class="card-text"> Attack Power = ` + allCharacters[character]["Attack Power"] + `</p>`
+
+    // $("#playerDiv").append(playerCard);
+
+
+
+
